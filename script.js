468e6da26dc55f21873ef94730ebb4b938b41dc6
harshithnetha1 <harshithnetha1@gmail.com>
2024-07-16 02:36:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var actualNumber=Math.floor(Math.random()*10)+1;
+let score=20;
+document.querySelector('.sub').addEventListener('click',function(){
+   
+   
+    let guessNumber=document.querySelector(".guess").value;
+    console.log(guessNumber);
+    if(!guessNumber){
+        document.querySelector('.statement').textContent="Enter A valid Number Other than 0 in between 1 to 20";
+        score=score-1;
+        document.querySelector('.score').textContent=score;
+    }
+    else if (actualNumber > guessNumber){
+        document.querySelector('.statement').textContent="Your number is Smaller than Actual Number";
+        score=score-1;
+        document.querySelector('.score').textContent=score;
+    }
+    else if (actualNumber < guessNumber){
+        document.querySelector('.statement').textContent="Your number is greater than Actual Number";
+        score=score-1;
+        document.querySelector('.score').textContent=score;
+
+    }
+    else if (actualNumber == guessNumber){
+        document.querySelector('.statement').textContent="Congratulations";
+        score=score;
+        document.querySelector('.score').textContent=score;
+        document.querySelector('.scoreMessage').textContent=`Your Highest Score `
+        
+
+    }
+    
+    
+    
+
+})
